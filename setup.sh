57717322ccbc73f1e388cc3649399881c427e7c8
t-hayashi <t.hayashi.dev@gmail.com>
2023-02-23 15:43:04 +0900

    update

--- setup.sh
+++ setup.sh
@@ -1,19 +1,13 @@
 git clone https://github.com/w0ng/vim-hybrid.git
 mkdir -p ~/.vim/colors
-mv ~/dot/vim-hybrid/colors/hybrid.vim ~/.vim/colors/
-rm -rf vim-hybrid/
+mv $(PWD)/vim-hybrid/colors/hybrid.vim ~/.vim/colors/
+rm -rf $(PWD)/vim-hybrid/
 
-ln -sf ~/dot/.vimrc ~/.vimrc
-ln -sf ~/dot/.zshenv ~/.zshenv
-ln -sf ~/dot/.zshrc ~/.zshrc
+ln -sf $(PWD)/.vimrc ~/.vimrc
+ln -sf $(PWD)/.zshenv ~/.zshenv
+ln -sf $(PWD)/.zshrc ~/.zshrc
 
-ln -sf ~/dot/.tmux.conf ~/.tmux.conf
-mkdir -p ~/.tmux.d
-ln -sf ~/dot/.tmux.d/mouse-before-2.0.conf ~/.tmux.d/
-ln -sf ~/dot/.tmux.d/mouse-after-2.1.conf ~/.tmux.d/
+ln -sf $(PWD)/.gitconfig ~/.gitconfig
 
-ln -sf ~/dot/.minttyrc ~/.minttyrc
-ln -sf ~/dot/.gitconfig ~/.gitconfig
-
-ln -sf ~/dot/.hammerspoon ~/.hammerspoon
-ln -sf ~/dot/.config ~/.config
+ln -sf $(PWD)/.hammerspoon ~/.hammerspoon
+ln -sf $(PWD)/.config ~/.config
